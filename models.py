4afe73f81f10809bfea04cf07c720c4af0ae3c29
gseemann <gabrielseemann@gmail.com>
2020-07-27 16:53:45 -0700

    matplotlib commented out

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from math import sin, cos, sqrt, atan2, radians
 
