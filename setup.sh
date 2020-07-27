35bf49b0f8fc1e75ad526ee164980909ba49a22b
gseemann <gabrielseemann@gmail.com>
2020-07-27 16:08:03 -0700

    setup.sh changes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "\
+
+echo “\
 [general]\n\
-email = \"gabrielseemann@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
+email = \”gabrielseemann@gmail.com\”\n\
+“ > ~/.streamlit/credentials.toml
+
+echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
