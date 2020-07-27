98e0679d1e39175d8a66e7cdb61c2c2d80b7a113
gseemann <gabrielseemann@gmail.com>
2020-07-27 14:27:16 -0700

    heroku files added

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"gabrielseemann@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+
